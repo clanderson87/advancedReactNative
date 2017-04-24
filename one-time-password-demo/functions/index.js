066fc991bc8b6fb447cddc81c2ae02a2fdc369bb
Chris Anderson <cl.anderson8@gmail.com>
2017-04-23 22:24:49 -0500

    backend complete

--- one-time-password-demo/functions/index.js
+++ one-time-password-demo/functions/index.js
@@ -3,6 +3,7 @@ const functions = require('firebase-functions');
 const createUser = require('./create_user');
 const serviceAccount = require('./service_account.json');
 const requestOneTimePassword = require('./request_one_time_password');
+const verifyOneTimePasswords = require('./verify_one_time_passwords');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -11,4 +12,5 @@ admin.initializeApp({
 
 exports.createUser = functions.https.onRequest(createUser);
 exports.requestOneTimePassword = functions.https.onRequest(requestOneTimePassword);
+exports.verifyOneTimePasswords = functions.https.onRequest(verifyOneTimePasswords);
 
